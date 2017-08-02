70d3f5fe9c1a4f2f5ab121bf806300107cd6d6c5
gittymutt <pedroschmitz@hotmail.com>
2017-08-02 17:05:52 -0400

    Begin dislay object

---
+++ display.js
@@ -0,0 +1,11 @@
+var display = {
+  isIrregular: false,
+  subj: "subjError",
+  BFV: "BFVerror",
+  sPast: "sPasterror",
+  theRest: "theResterror"
+}
+
+display.init = function(myForm) {
+  console.log(myForm.BFV);
+}
--- trainer.js
+++ trainer.js
@@ -5,5 +5,7 @@ while (!form.done) {
   if (form.newForm) {console.log(form.formName);}
   if (form.newSentence) {console.log("new sentence");}
   console.log(" returned from getWord(): " + form.getWord());
-  console.log(form.getPosition());
+  //console.log(form.getPosition());
 }
+
+display.init(form);
