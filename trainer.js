4ff503f28599380c72a1a90a1c9c56cadf512a91
gittymutt <pedroschmitz@hotmail.com>
2017-08-04 22:50:57 -0400

    Notes for to do

--- trainer.js
+++ trainer.js
@@ -1,13 +1,5 @@
 form.init();
-/*
-console.log("form name at first: " + form.formName);
-while (!form.done) {
-  if (form.newForm) {console.log(form.formName);}
-  if (form.newSentence) {console.log("new sentence");}
-  console.log(" returned from getWord(): " + form.getWord());
-  //console.log(form.getPosition());
-}
-*/
+
 display.init(form);
 display.setUpButtons();
 display.setUpForm();
@@ -15,7 +7,4 @@ display.setUpForm();
 
 
 // to do:
-// add simple present hesheit form
-// put in irreg on keyboard
-
-// make all things like Spast, SPast etc. the same
+// change buttonArray to dictionary
