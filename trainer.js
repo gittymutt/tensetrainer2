abd9efdd87e25b876459f44cca2a1f82d83919ec
gittymutt <pedroschmitz@hotmail.com>
2017-08-20 11:29:23 -0400

    update to do list in trainer.js

--- trainer.js
+++ trainer.js
@@ -8,10 +8,18 @@ display.setUpForm();
 
 // to do:
 // Put in word choices
-// put question mark at end when question
+// put question mark at end when question **DONE**
 
 // Make right/wrong signs
 // figure out where to put right sign when sentence is completed
 
-// fix correct sign. It shows next form before current
+// fix correct sign. It shows next verb before current
 // correct answer
+
+// add verb + ed put together
+
+// add be verbs
+
+// Fix layout and make mobile first friendly
+
+ 
